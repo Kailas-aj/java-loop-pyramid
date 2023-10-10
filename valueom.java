6996c74c33d27d55cdd517b906b00e5a2f19cb24
Kailas-aj <137808288+Kailas-aj@users.noreply.github.com>
2023-10-11 00:05:20 +0530

    my java

---
+++ valueom.java
@@ -0,0 +1,29 @@
+package kailas;
+
+public class valueom {
+
+	public static void main(String[] args) {
+		
+		 for(int i=1;i<=10;i++) {
+			 
+		 for (int t=10;t>=i;t--) {
+			 System.out.print("  ");
+		 }
+		
+			 for(int j=1;i>=j;j++) {
+				 System.out.print("   *");
+				 
+			 }
+			 System.out.println();
+			 
+		 }
+ 
+			
+			}
+			
+}
+	
+
+	
+
+
